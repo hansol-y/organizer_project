c6f3ca7564795361aeb94eaaaf04fb41a54602c6
hansolyang <hansol94@student.ubc.ca>
2024-02-08 20:37:52 +0900

    added userModel schema

---
+++ server/models/userModel.js
@@ -0,0 +1,41 @@
+const { int, string } = require("blink-detection");
+const mongoose = require("mongoose");
+
+const dbUser = process.env.DB_USERNAME;
+const dbPass = process.env.DB_PASSWORD;
+
+const connectionString = 'mongodb+srv://${dbUser}${dbPass}@cluster-moodvec.sryz4qe.mongodb.net/?retryWrites=true&w=majority';
+
+mongoose.connect(connectionString);
+
+// TODO: finalize the mood schema
+
+const userSchema = mongoose.Schema(
+    {
+        userId: { // auto-create
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            index: true,
+            auto: true
+        },
+        userName: { 
+            type: string,
+            required: true
+        },
+        password: { // x coord
+            type: string,
+            required: true
+        },
+        email: {
+            type: string,
+            required: true
+        },
+        moods: {
+            type: [moodSchema],
+            required: true,
+            default: []
+        }
+    }
+);
+
+module.exports = mongoose.model("User", userSchema);
