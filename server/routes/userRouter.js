6d96f4d6fe51cf809c1826ba3e78743ecc3350be
hansolyang <hansol94@student.ubc.ca>
2024-02-08 20:44:15 +0900

    added user POST api

---
+++ server/routes/userRouter.js
@@ -0,0 +1,25 @@
+const User = require('./models/userModel');
+const router = require('./router');
+
+router.post('/user', async (req, res) => {
+    try {
+        const { userName, password, email } = req.body;
+
+        const newUser = new User({
+            userName,
+            password,
+            email
+        });
+
+        await newUser.save();
+
+        res.status(201).json("Saved");
+
+    } catch(err) {
+        res.status(500).send({error: err});
+    }
+});
+
+router.put('/user/{$password}', async (req, res) => {
+    // TODO: Update password + update user email as next put API
+})
