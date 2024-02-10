ba0e0a63386ce0c692c85af3c218108cc68e4a54
hansolyang <hansol94@student.ubc.ca>
2024-02-10 18:01:29 +0900

    added user PUT, DELETE API

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -20,6 +20,36 @@ router.post('/user', async (req, res) => {
     }
 });
 
-router.put(`/user/{$password}`, async (req, res) => {
-    // TODO: Update password + update user email as next put API
+router.put('/user/update-password', async (req, res) => {
+    try {
+        const {username, password} = req.headers;
+        const {currentPassword, newPassword} = req.body;
+        User.update({userName: username, password: currentPassword}, {password: newPassword});
+        // TODO: deal with the response
+    } catch(error) {
+    }
+});
+
+router.put('/user/update-email', async (req, res) => {
+    try {
+        const {username, password} = req.headers;
+        const {currentEmail, newEmail} = req.body;
+        User.update({userName: username, password: password, email: currentEmail}, {email: newEmail});
+        // TODO: deal with the response
+    } catch(error) {
+
+    }
+});
+
+router.delete('user', async (req, res) => {
+    try {
+        const {userName, password} = req.headers;
+        User.deleteOne({userName: userName, password: password});
+
+        if (res.status !== 201) {
+            throw new Error();
+        }
+    } catch(error) {
+        
+    }
 })
