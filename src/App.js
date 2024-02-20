5c3ebd16805a9fd51a522e68cc2b497f0e72dedb
hansolyang <hansol94@student.ubc.ca>
2024-02-20 19:51:40 +0900

    implemented initial page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import logo from './assets/coordinate_s.png';
 import './App.css';
-import SignUp from './SignUp';
-import SignIn from './SignIn';
+import SignUp from './SignUp.js';
+import SignIn from './SignIn.js';
+import Main from './Main.js'
 
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
@@ -10,26 +10,20 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo pop" alt="logo" />
-          <p className="Title">
-            Mind Vector
-          </p>
-          <p className="description">
-            Draw your mood vector to let you know yourself better and find the best direction
-          </p>
-        </header>
+        
+        <Routes>
+          <Route path="/" exact element={<Main />} />
+          <Route exact path="/SignUp" element={<SignUp />} />
+          <Route exact path="/SignIn" element={<SignIn />} />
+        </Routes>
 
-        <nav>
-          <Link to="/signup">Sign Up</Link>
-          <Link to="/signin">Sign In</Link>
+        <nav className="Main-signUp-signIn">
+          <Link to="/SignUp">Sign Up</Link>
+          <Link to="/SignIn">Sign In</Link>
         </nav>
 
         <hr />
-        <Routes>
-          <Route exact path="/signup" component={SignUp} />
-          <Route exact path="/signin" component={SignIn} />
-        </Routes>
+        
         
 
       </div>
--- src/SignIn.js
+++ src/SignIn.js
@@ -1,5 +1,52 @@
 // TODO: Organize directory of the js files
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
-function SignIn() {
+import './App.css';
+
+const backendPort = process.env.BACKEND_PORT;
+const serverBaseUrl = `http://localhost:${backendPort}`
+const userApiEndpoint = `${serverBaseUrl}/api/user`
+
+// const { userName, password } = req.headers;
+async function signIn(username, password) {
+    try {
+        const response = await fetch(`${userApiEndpoint}/signin`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'userName': username,
+                'password': password
+            }
+        });
+
+        if (response.status !== 201) {
+            const error = await response.json();
+            throw new Error(`Sign in failed: ${error}`);
+        } else {
+            const successContainer = document.getElementById('success-container');
+            successContainer.innerHTML = `<p>Success: ${await response.json().then((result) => result.message)}</p>`;
+        }
+    } catch(err) {
+
+    }
+}
+
+const SignIn = () => {
+    return (
+        // TODO: rendering
+        <Router>
+            <div className='sign-in'>
+                <header className='sign-in-header'>
+                    <p className='sign-in-title'>
+                        Sign In
+                    </p>
+                </header>
+            </div>
+
+        </Router>
+    );
 
 }
+
+export default SignIn;
