e320d06d0d665a0633380c31268f5e16dee420d8
hansolyang <hansol94@student.ubc.ca>
2024-02-24 23:08:09 +0900

    fixed bugs in userRouter

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,18 +1,22 @@
-const User = require('./models/userModel');
+
+const express = require("express");
+const User = require('../models/userModel');
 const bcrypt = require('bcrypt');
 
-const router = require('./router');
+const router = express.Router();
+
+const saltRound = 10;
 
 // Sign up API
-router.post('/user/signup', async (req, res) => {
+router.post('/signup', async (req, res) => {
     try {
         const { userName, password, email } = req.body;
-        const hashedPassword = bcrypt.hash(password, 10);
+        let hashedPassword = await bcrypt.hash(password, saltRound);
 
         const newUser = new User({
-            userName,
-            hashedPassword,
-            email
+            userName: userName,
+            password: hashedPassword,
+            email: email
         });
 
         await newUser.save();
@@ -28,12 +32,12 @@ router.post('/user/signup', async (req, res) => {
 });
 
 // Update password
-router.put('/user/update-password', async (req, res) => {
+router.put('/update-password', async (req, res) => {
     try {
         const {username} = req.headers;
-        const newPassword = req.body;
-        const newHashedPW = bcrypt.hash(newPassword, 10);
-        await User.update({userName: username}, {password: newHashedPW});
+        const {newPassword} = req.body;
+        const newHashedPW = await bcrypt.hash(newPassword, saltRound);
+        await User.findOneAndUpdate({ userName: username }, { password: newHashedPW});
 
         res.status(201).json({
             message: 'Successfully updated user password'
@@ -44,32 +48,36 @@ router.put('/user/update-password', async (req, res) => {
 });
 
 // Update email
-router.put('/user/update-email', async (req, res) => {
+router.put('/update-email', async (req, res) => {
     try {
         const {username} = req.headers;
         const {currentEmail, newEmail} = req.body;
-        const user = await User.update({userName: username, email: currentEmail}, {email: newEmail});
+        const user = await User.findOne({ userName: username });
+
         if (!user) {
             res.status(404).json({
                 message: 'The user does not exist.',
                 user: user.toObject()
             });
-        } else {
-            res.status(201).json({
-                message: 'Successfully updated user email',
-                previous_email: currentEmail,
-                new_email: newEmail
-            });
+            return res;
         }
+
+        await User.findOneAndUpdate({ userName: username }, { email: newEmail});
+        res.status(201).json({
+            message: 'Successfully updated user email',
+            previous_email: currentEmail,
+            new_email: newEmail
+        });
+
     } catch(err) {
         return res.status(500).json({error: err.message});
     }
 });
 
 // DELETE user account
-router.delete('user', async (req, res) => {
+router.delete('', async (req, res) => {
     try {
-        const {userName, password} = req.headers;
+        const { userName, password } = req.body;
         const user = await User.findOne({userName: userName});
 
         if (!user) {
@@ -78,7 +86,7 @@ router.delete('user', async (req, res) => {
                 error: "User with matching user ID does not exist"
             })
         } 
-        const passwordMatch = bcrypt.compare(password, user.password);
+        const passwordMatch = await bcrypt.compare(password, user.password);
 
         if (!passwordMatch) {
             return res.status(401).json({
@@ -87,29 +95,30 @@ router.delete('user', async (req, res) => {
             })
         }
 
-        await User.deleteById(user.userId);
+        await User.deleteOne({userId: user.userId});
         res.status(201).send("Successfully deleted the user data.");
 
         // TODO: Add authorization for ensuring the user is signed in
 
-    } catch(error) {
-        return res.status(500).send(error);
+    } catch(err) {
+        return res.status(500).json({error: err.message});
     }
 });
 
 // Sign in API
-router.get('user/signin', async (req, res) => {
+router.get('/signin', async (req, res) => {
     try {
-        const { userName, password } = req.headers;
+        const { userName, password } = req.body;
+        console.log(`Finding user: ${userName}`);
         const user = await User.findOne({ userName: userName});
 
         if (!user) {
-            return res.status(401).json({
+            return res.status(404).json({
                 message: "Sign in failed.",
                 error: "Invalid Id"
             });
         }
-        const passwordMatch = bcrypt.compare(password, user.password);
+        const passwordMatch = await bcrypt.compare(password, user.password);
         if (!passwordMatch) {
             return res.status(401).json({
                 message: "Sign in failed",
@@ -123,4 +132,4 @@ router.get('user/signin', async (req, res) => {
     }
 });
 
-module.exports = userRouter;
+module.exports = router;
