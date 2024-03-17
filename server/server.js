942d101f4cc1ad07744288bc5fc593f0ca6956b6
hansolyang <hansol94@student.ubc.ca>
2024-03-17 14:38:56 +0900

    added server file to run the backend

---
+++ server/server.js
@@ -0,0 +1,32 @@
+require('dotenv').config({path: '../.env'});
+
+
+var express = require("express");
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+var app = express();
+const PORT = process.env.BACKEND_PORT || 8000;
+
+const dbUser = process.env.DB_USERNAME;
+const dbPass = process.env.DB_PASSWORD;
+
+const userRouter = require('./routes/userRouter');
+const moodRouter = require('./routes/moodRouter');
+
+const connectionString = `mongodb+srv://${dbUser}:${dbPass}@cluster-moodvec.sryz4qe.mongodb.net/moodVec_db?retryWrites=true&w=majority`;
+
+mongoose.connect(connectionString).then(() => {
+    console.log('Connected to MongoDB: moodVec_db');
+}).catch((err) => {
+    console.error('Failed to connect to MongoDB: ', err);
+});
+
+app.use(bodyParser.json());
+app.use('/api/user', userRouter);
+app.use('/api/mood', moodRouter);
+
+
+app.listen(PORT, () => {
+    console.log(`Running the server on port ${PORT}`);
+})
