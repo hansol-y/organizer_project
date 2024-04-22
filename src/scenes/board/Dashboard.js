ec6cbd02a7a698d5ed475a89d7d611288118a1a5
hansolyang <hansol94@student.ubc.ca>
2024-04-22 17:24:05 +0900

    rendered dashboard page

--- src/scenes/board/Dashboard.js
+++ src/scenes/board/Dashboard.js
@@ -1,61 +1,48 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import axios from 'axios';
 
 import logo from '../../logo.svg';
+import user_img from '../../assets/user.png'
 
-const backendPort = process.env.BACKEND_PORT;
-const serverBaseUrl = `http://localhost:${backendPort}`;
-const userApiEndpoint = `${serverBaseUrl}/api/user`;
-const moodApiEndpoint = `${serverBaseUrl}/api/mood`;
+import './Board.css';
 
-const getTodaysMood = async (userId) => {
+const userApiEndpoint = "api/user";
+const moodApiEndpoint = "api/mood";
+
+const getTodaysMood = async (_id) => {
     const today = new Date();
     const date = `${today.getFullYear()}-${today.getMonth()}-${today.getDate()}`
     try {
-        const response = await axios.get(`/date/${date}`, {
+        const response = await axios.get(`${moodApiEndpoint}/date/${date}`, {
             headers: {
-                userId: userId
+                _id: _id
             }
         });
 
-        if (response.status == 204) {
+        if (response.status === 204) {
             // no contents
             return;
-        } else if (response.status == 201) {
+        } else if (response.status === 201) {
             return response.body; // getting list of moods in today
         } else {
             throw Error(response);
         }
-
-
     } catch (error) {
         alert(`Failed to load today's mood: ${error.message}`);
     }
 };
 
-const dashboard = async () => {
-    // const navigate = useNavigate();
+const Dashboard = () => {
+    const navigate = useNavigate();
     const location = useLocation();
-    const user = location.state.user;
+    console.log("Getting user data from react-router-dom location");
+    console.log(location);
+    const { userId, password, username, email } = location.state;
 
-    const createMoodReq = () => {
-        // navigate("/Create");
+    const navigateAddMood = () => {
+        navigate('/Create', {state: {userId: userId, password: password, username: username, email: email}});
     }
-
-    const moveToCoordinate = () => {
-        // navigate("/Coor")
-    }
-
-    const moveToHistory = () => {
-        // navigate("/History")
-    }
-
-    const moveSuggestions = () => {
-        // navigate("/Suggestion")
-    }
-
-    const todaysData = await getTodaysMood();
 
     return (
         <div className='dashboard'>
@@ -64,26 +51,18 @@ const dashboard = async () => {
                     Dashboard
                 </h1>
             </header>
-            <div className='sidemenu'>
-                <img src={logo} className="small-logo" alt="logo" />
-                <div className='menu-list'>
-                    <ul id='menu-content' className='menu-content'>
-                        <li>
-                            <a href='./scenes/mood/createMood.js'>
-                                <i className='menu'>Mood History</i>
-                            </a>
-                        </li>
-                        
-                    </ul>
-                </div>
-            </div>
+            
             <div className='coordinate'>
-
+                <hr className='coord-hr' />
+                <hr className='coord-vr' />
             </div>
+            <button className='add-mood' onClick={navigateAddMood}>
+                Add Mood
+            </button>
         </div>
-    )
+    );
 
 
 }
 
-export default dashboard;
+export default Dashboard;
---
+++ src/scenes/board/Default.js
@@ -0,0 +1,81 @@
+
+import React from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+
+import user_img from '../../assets/user.png'
+
+import './Board.css';
+
+const Default = ({ children }) => {
+    const navigate = useNavigate();
+    const location = useLocation();
+    const { userId, password, username, email } = location.state;
+
+    const signOut = () => {
+        navigate('/', {state: null});
+    }
+
+    const navigateMyPage = () => {
+        navigate('/MyPage', {state: {userId: userId, password: password, username: username, email: email}});
+    }
+
+    const navigateDashboard = () => {
+        navigate('/Dashboard', {state: {userId: userId, password: password, username: username, email: email}});
+    }
+
+    const navigateCoord = () => {
+        navigate('/Coord', {state: {userId: userId, password: password, username: username, email: email}});
+    }
+
+    const navigateHistory = () => {
+        navigate('/History', {state: {userId: userId, password: password, username: username, email: email}});
+    }
+
+    const navigateSuggestion = () => {
+        navigate('/Suggestion', {state: {userId: userId, password: password, username: username, email: email}});
+    }
+
+    return (
+        <div className="default">
+            <div className='sidebar'>
+                <nav>
+                    <ul className='sidemenu'>
+                        <li onClick={navigateDashboard}>
+                            Dashboard
+                        </li>
+                        <li onClick={navigateCoord}>
+                            Coordinate
+                        </li>
+                        <li onClick={navigateHistory}>
+                            Mood History
+                        </li>
+                        <li onClick={navigateSuggestion}>
+                            Suggestion
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <div className='my-info-side'>
+                <ul>
+                    <li className='menu-hover' onClick={navigateMyPage}>
+                        My Page
+                    </li>
+                    <li className='menu-hover' onClick={signOut}>
+                        Sign Out
+                    </li>
+                    <li>
+                        <img src={user_img} className='user-img-info-side' />
+                    </li>
+                    <li>
+                        Hello, {username}!
+                    </li>
+                </ul>
+            </div>
+            <div className='content'>
+                {children}
+            </div>
+        </div>
+    );
+}
+
+export default Default;
