0d59f1213f4195c06c7b3cb942ce2bf20040f73b
hansolyang <hansol94@student.ubc.ca>
2024-04-22 17:25:55 +0900

    rendered mood creation page

--- src/scenes/mood/createMood.js
+++ src/scenes/mood/createMood.js
@@ -1,6 +1,7 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import axios from 'axios';
+import {Formik, Field, Form} from 'formik';
 
 import logo from '../../logo.svg';
 
@@ -9,11 +10,13 @@ const serverBaseUrl = `http://localhost:${backendPort}`;
 const userApiEndpoint = `${serverBaseUrl}/api/user`;
 const moodApiEndpoint = `${serverBaseUrl}/api/mood`;
 
-const createMood = async() =>  {
+const CreateMood = () =>  {
 
     const navigate = useNavigate();
     const location = useLocation();
     const user = location.state.user;
+
+    const date = Date.now();
 
     const postMood = async(mood, strength, personal, activeness, date) => {
         try {
@@ -27,11 +30,11 @@ const createMood = async() =>  {
             },
             {
                 headers: {
-                    userId: user.userId
+                    _id: user._id
                 }
             });
 
-            if (response.status != 201) {
+            if (response.status !== 201) {
                 throw new Error(response.data);
             } else {
                 alert("New mood is successfully created!");
@@ -42,6 +45,11 @@ const createMood = async() =>  {
         }
     }
 
+    const handleForMoodSubmit = async (values) => {
+        const {mood, strength, personal, activeness, date} = values;
+        postMood(mood, strength, personal, activeness, date);
+    }
+
     return (
         <div className='mood-input'>
             <header className='mood-input-header'>
@@ -49,10 +57,50 @@ const createMood = async() =>  {
                     Add Your Current Mood!
                 </h1>
             </header>
-            <div className=''>
+            <div className='mood-form'>
+                <Formik initialValues={{
+                    mood: "",
+                    strength: 0,
+                    personal: 0,
+                    activeness: 0,
+                    date: null
+                }}
+                    onSubmit={handleForMoodSubmit}>
+                    {
+                        <Form className='mood-create-form'>
+                            <label htmlFor='mood'>Mood</label>
+                            <br />
+                            <Field name="mood" />
+                            <br />
+                            <br />
+                            <label htmlFor='strength'>Strength (In 0-5)</label>
+                            <br />
+                            <Field name="strength" />
+                            <br />
+                            <br />
+                            <label htmlFor='personal'>Personal (In 0-5)</label>
+                            <br />
+                            <Field name="personal" />
+                            <br />
+                            <br />
+                            <label htmlFor='activeness'>Activeness (In 0-5)</label>
+                            <br />
+                            <Field name="activeness" />
+                            <br />
+                            <br />
+                            <label htmlFor='date'>Date</label>
+                            <br />
+                            <Field name="date" />
+                            <br />
+
+                            <button className='submit' type='submit'>Add Mood</button>
+                        </Form>
+                    }
+
+                </Formik>
             </div>
         </div>
     )
 }
 
-export default createMood;
+export default CreateMood;
