af31a555c45df4439b5b211e4b1f50e0332bb901
hansolyang <hansol94@student.ubc.ca>
2024-06-04 17:07:30 +0900

    added history lookup

--- src/scenes/mood/MoodHistory.js
+++ src/scenes/mood/MoodHistory.js
@@ -1,15 +1,32 @@
 import React, { useState } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import {Formik, Field, Form} from 'formik';
-import axios from 'axios';
 
-import logo from '../../assets/coordinate_s.png';
+import { getMonthlyMoods, countMoods } from '../../utils/HistoryUtils';
 
-const moodApiEndpoint = "api/mood";
+
+const backendUrl = process.env.REACT_APP_BACKEND_URL;
+const serverBaseUrl = `${backendUrl}`;
 
 const MoodHistory = () => {
-    
-    // here user can get the statistics data based on month
+
+    const navigate = useNavigate();
+    const location = useLocation();
+    const { userId, password, username, email, token } = location.state;
+
+    const date = new Date();
+
+    const handleForSubmit = async (values, onSubmitProps) => {
+        const {month, year} = values;
+
+        const records = getMonthlyMoods(month, year);
+        const count = countMoods(records);
+
+        onSubmitProps.setSubmitting(true);
+
+        return count;
+    }
+
 
     return (
         <div className='mood-history'>
@@ -18,8 +35,35 @@ const MoodHistory = () => {
                     Your Mood History
                 </h1>
             </header>
-            <div className=''>
-            </div>
+            <p>
+                Please select the month and year of the data you want to bring
+            </p>
+            <Formik initialValues={{
+                month: date.getMonth() + 1,
+                year: date.getFullYear()
+            }}
+                onSubmit={handleForSubmit}>
+                {({isSubmitting}) => (
+                    <Form className='mood-date-form'>
+                        <label htmlFor='month'>Month</label>
+                        <br />
+                        <Field name="month" />
+                        <br />
+
+                        <label htmlFor='year'>Year</label>
+                        <br />
+                        <Field name="year" />
+                        <br />
+
+                        <button className="year" type="submit" disabled={isSubmitting}>Submit</button>
+                    </Form>
+                )}
+            </Formik>
+            {isSubmitting || values.count ? (
+                <div className='display-moods'>
+
+                </div>
+                ) : null}
         </div>
     )
 }
---
+++ src/utils/HistoryUtils.js
@@ -0,0 +1,71 @@
+import useLocation from 'react-router-dom';
+
+const location = useLocation();
+import axios from 'axios';
+
+const { userId, password, username, email, token } = location.state;
+
+const backendUrl = process.env.REACT_APP_BACKEND_URL;
+const moodApiEndpoint = `${backendUrl}/api/mood`;
+
+export const getMonthlyMoods = async (month, year) => {
+    try {
+        const response = await axios.get(`${moodApiEndpoint}/month`,
+        {},
+        {
+            headers: {
+                Authorization: `${token}`
+            },
+            params: {
+                month: month,
+                year: year
+            }
+        });
+
+        if (response.status === 201 || response.status === 204) {
+            return response.data;
+        } else {
+            throw Error("Unable to load mood data. Please try again.");
+        }
+    } catch(error) {
+        console.log(error.message);
+    }
+}
+
+export const countMoods = (records) => {
+    let counts = {
+        happy: 0,
+        sad: 0,
+        angry: 0,
+        anxious: 0,
+        calm: 0,
+        energetic: 0
+    }
+
+    for (const record of records) {
+        switch(record.mood) {
+            case "happy":
+                counts.happy += 1;
+                break;
+            case "sad":
+                counts.sad += 1;
+                break;
+            case "angry":
+                counts.angry += 1;
+                break;
+            case "anxious":
+                counts.anxious += 1;
+                break;
+            case "calm":
+                counts.calm += 1;
+                break;
+            case "energetic":
+                counts.energetic += 1;
+                break;
+            default:
+                // ignore the record
+                break;
+        }
+    }
+    return counts;
+}
