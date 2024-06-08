fbdf939ee8306f1dcfc3d4a4756c1cff1be48123
hansolyang <hansol94@student.ubc.ca>
2024-06-08 14:03:33 +0900

    added chart in mood history, rearragned codes in history & suggestions

--- src/scenes/mood/MoodHistory.js
+++ src/scenes/mood/MoodHistory.js
@@ -1,30 +1,96 @@
-import React, { useState } from 'react';
-import { useNavigate, useLocation } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import {Formik, Field, Form} from 'formik';
-
-import { getMonthlyMoods, countMoods } from '../../utils/HistoryUtils';
+import Chart from 'chart.js/auto';
+import axios from 'axios';
 
 
 const backendUrl = process.env.REACT_APP_BACKEND_URL;
 const serverBaseUrl = `${backendUrl}`;
+const moodApiEndpoint = `${serverBaseUrl}/api/mood`;
 
 const MoodHistory = () => {
 
-    const navigate = useNavigate();
     const location = useLocation();
     const { userId, password, username, email, token } = location.state;
 
+    const countMoods = (records) => {
+        return records.reduce((acc, record) => {
+            if (acc.hasOwnProperty(record.mood)) {
+                acc[record.mood] += 1;
+            }
+            return acc;
+        }, {
+            happy: 0,
+            sad: 0,
+            angry: 0,
+            anxious: 0,
+            calm: 0,
+            energetic: 0
+        });
+    };
+
     const date = new Date();
 
-    const handleForSubmit = async (values, onSubmitProps) => {
+    const getMonthlyMoods = async (month, year) => {
+        try {
+            const response = await axios.get(`${moodApiEndpoint}/month`,
+            {
+                headers: {
+                    Authorization: `${token}`
+                },
+                params: {
+                    month: month,
+                    year: year
+                }
+            });
+    
+            if (response.status === 201) {
+                return response.data;
+            } else if (response.status === 204) {
+                throw Error("No data exists.");
+            } else {
+                throw Error("Unable to load mood data. Please try again.");
+            }
+        } catch(error) {
+            alert(error.message);
+        }
+    }
+
+    const handleForSubmit = async (values) => {
         const {month, year} = values;
+        const records = await getMonthlyMoods(month, year);
+        const count = await countMoods(records);
+        drawPieChart(count, month, year);
+    }
 
-        const records = getMonthlyMoods(month, year);
-        const count = countMoods(records);
+    const drawPieChart = (count, month, year) => {
+        console.log("Drawing pie chart");
+        console.log(count, month, year);
+        const data = {
+            labels: [
+                "happy",
+                "sad",
+                "angry",
+                "anxious",
+                "calm",
+                "energetic"
+            ],
+            datasets: [{
+                label: `My Mood Graph in ${month}/${year}`,
+                data: [count.happy, count.sad, count.angry, count.anxious, count.calm, count.energetic]
+            }]
+        };
 
-        onSubmitProps.setSubmitting(true);
-
-        return count;
+        console.log(data);
+        
+        const ctx = document.getElementById('pie-chart').getContext('2d');
+        new Chart(ctx,
+            {
+                type: 'pie',
+                data: data
+            }
+        )
     }
 
 
@@ -43,7 +109,7 @@ const MoodHistory = () => {
                 year: date.getFullYear()
             }}
                 onSubmit={handleForSubmit}>
-                {({isSubmitting}) => (
+                {
                     <Form className='mood-date-form'>
                         <label htmlFor='month'>Month</label>
                         <br />
@@ -55,15 +121,13 @@ const MoodHistory = () => {
                         <Field name="year" />
                         <br />
 
-                        <button className="year" type="submit" disabled={isSubmitting}>Submit</button>
+                        <button className="year" type="submit">Submit</button>
                     </Form>
-                )}
+                }
             </Formik>
-            {isSubmitting || values.count ? (
-                <div className='display-moods'>
-
+                <div className='pie-chart'>
+                    <canvas id='pie-chart'></canvas>
                 </div>
-                ) : null}
         </div>
     )
 }
--- src/scenes/mood/Suggestion.js
+++ src/scenes/mood/Suggestion.js
@@ -5,9 +5,47 @@ import axios from 'axios';
 
 import logo from '../../assets/coordinate_s.png';
 
-const moodApiEndpoint = "api/mood";
+import { predictNextMood } from '../../utils/SuggestionUtils';
+
+const backendUrl = process.env.REACT_APP_BACKEND_URL;
+const serverBaseUrl = `${backendUrl}`;
+const moodApiEndpoint = `${serverBaseUrl}/api/mood`;
 
 const Suggestion = () => {
+
+    const location = useLocation();
+    const { userId, password, username, email, token } = location.state;
+    let latest = null;
+
+    const getPrediction = async() => {
+        const today = new Date();
+        try {
+            const response = await axios.get(`${moodApiEndpoint}/month`,
+            {
+                headers: {
+                    Authorization: `${token}`
+                },
+                params: {
+                    month: today.getMonth() + 1,
+                    year: today.getFullYear()
+                }
+            });
+
+            if (response.status === 201) {
+                const predictedClass = predictNextMood(response.data);
+                console.log(predictedClass);
+                return predictedClass;
+            } else if (response.status === 204) {
+                throw Error("You haven't recorded any feelings this month. We provide the suggestion based on your mood records in a month.")
+            } else {
+                throw Error()
+            }
+        } catch(error) {
+            alert(error.message);
+        }
+    }
+
+    getPrediction();
 
     return (
         <div className='mood-history'>
@@ -16,7 +54,19 @@ const Suggestion = () => {
                     Suggestions on Your Current Mood
                 </h1>
             </header>
-            <div className=''>
+            <div className='predict-mood'>
+                Your latest mood was...
+                <div className='present-mood'>
+
+                </div>
+                
+                <br />
+                <br />
+
+                Based on your monthly mood trend, your next mood might be...
+                <div className='present-prediction'>
+
+                </div>
             </div>
         </div>
     )
